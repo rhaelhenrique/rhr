e0565cd3791c5757bf65c64f1844b8d891d886c5
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2021-04-18 16:09:50 -0300

    atualização rhr.net.br
    Layout rodando ok

--- pages/index.js
+++ pages/index.js
@@ -5,60 +5,63 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>RhR - Dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <top className={styles.top}>
+
+      <img src="/logo-RHR-1.png" alt="LOGO RHR" className={styles.logo2} />
+
+      </top>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Rhael Henrique
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h2>Full-stack Developer</h2>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <img src="/skills/html.png" alt="HTML5" className={styles.logo1} />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <img src="/skills/css.png" alt="CSS" className={styles.logo1} />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <img src="/skills/js.png" alt="Javascript" className={styles.logo1} />
+        
+          <img src="/skills/php.png" alt="PHP" className={styles.logo1} />
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <img src="/skills/python.png" alt="Python" className={styles.logo1} />
+
+          <img src="/skills/node.png" alt="Node Js" className={styles.logo1} />
+
+          <img src="/skills/next.png" alt="Next Js" className={styles.logo1} />
+
+          <img src="/skills/react.png" alt="React" className={styles.logo1} />
+
+          <img src="/skills/flutter.png" alt="Flutter" className={styles.logo1} />
+
+          <img src="/skills/android.png" alt="Android" className={styles.logo1} />
+
+          <img src="/skills/git.png" alt="Git" className={styles.logo1} />
+
+          <img src="/skills/sql.png" alt="SQL" className={styles.logo1} />
+          
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <h3>
+          <a
+            href="https://www.linkedin.com/in/rhaelhenrique/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/linkedin.webp" alt="Linkedin Logo" className={styles.logo} />
+            Linkedin{' '}
+            - Rhael Henrique
+          </a>
+        </h3>
       </footer>
     </div>
   )
