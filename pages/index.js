023a1cf4c875e06e7d843349861b1ecb0957054e
Rhael Henrique <rhaelhenriquerodrigues@gmail.com>
2021-05-24 11:50:39 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,10 @@ export default function Home() {
       </top>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Rhael Henrique
-        </h1>
 
-        <h2>Full-stack Developer</h2>
+        <h1 className={styles.title}>Rhael Henrique</h1>
+
+        <h2>Desenvolvedor</h2>
 
         <div className={styles.grid}>
           <img src="/skills/html.png" alt="HTML5" className={styles.logo1} />
@@ -28,26 +27,21 @@ export default function Home() {
           <img src="/skills/css.png" alt="CSS" className={styles.logo1} />
 
           <img src="/skills/js.png" alt="Javascript" className={styles.logo1} />
-        
-          <img src="/skills/php.png" alt="PHP" className={styles.logo1} />
-
-          <img src="/skills/python.png" alt="Python" className={styles.logo1} />
-
-          <img src="/skills/node.png" alt="Node Js" className={styles.logo1} />
-
-          <img src="/skills/next.png" alt="Next Js" className={styles.logo1} />
 
           <img src="/skills/react.png" alt="React" className={styles.logo1} />
 
-          <img src="/skills/flutter.png" alt="Flutter" className={styles.logo1} />
+          <img src="/skills/next.png" alt="Next Js" className={styles.logo1} />
 
-          <img src="/skills/android.png" alt="Android" className={styles.logo1} />
+          <img src="/skills/node.png" alt="Node Js" className={styles.logo1} />
 
           <img src="/skills/git.png" alt="Git" className={styles.logo1} />
 
           <img src="/skills/sql.png" alt="SQL" className={styles.logo1} />
+
+          <img src="/skills/php.png" alt="PHP" className={styles.logo1} />
           
         </div>
+        
       </main>
 
       <footer className={styles.footer}>
@@ -57,12 +51,25 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="/linkedin.webp" alt="Linkedin Logo" className={styles.logo} />
+            <img src="/linkedin.png" alt="Linkedin Logo" className={styles.logo} />
             Linkedin{' '}
-            - Rhael Henrique
+          </a>
+        </h3>
+        <h3>
+          <a
+            href="https://www.github.com/rhaelhenrique"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="/github.png" alt="GitHub Logo" className={styles.logo} id="logoGit"/>
+            GitHub{' '}
           </a>
         </h3>
       </footer>
     </div>
   )
 }
+
+//<img src="/skills/python.png" alt="Python" className={styles.logo1} />
+//<img src="/skills/flutter.png" alt="Flutter" className={styles.logo1} />
+//<img src="/skills/android.png" alt="Android" className={styles.logo1} />
